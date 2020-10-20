eea75fd1cfdb991917c13c577cf57d91131a2175
Anathug <anatoletouvron@hotmail.fr>
2020-10-20 13:56:38 +0200

    :construction: adding three  to build

--- src/assets/scene/ThreeScene.js
+++ src/assets/scene/ThreeScene.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import gsap from "gsap";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import holywoodImg from "../img/holywood.jpg";
 const createInputEvents = require("simple-input-events");
 
