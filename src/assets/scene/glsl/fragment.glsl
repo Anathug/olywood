69079f8cb49ed0c3d31939b6100162c430c6285d
Anathug <anatoletouvron@hotmail.fr>
2020-10-23 14:30:25 +0200

    :sparkles: shaders working perfectly

--- src/assets/scene/glsl/fragment.glsl
+++ src/assets/scene/glsl/fragment.glsl
@@ -21,16 +21,16 @@ void main() {
 	vec2 res = uRes * PR;
 	vec2 st = gl_FragCoord.xy / res.xy - vec2(0.5);
 	st.y *= uRes.y /uRes.x;
-    float normSpeed = clamp(speed * 100. ,0. ,1.); 
+    float normSpeed = clamp(speed * 50. ,0. ,1.); 
 
-	vec2 mouse = uMouse * -1.;
+	vec2 mouse = uMouse * -.5;
 	
 	vec2 circlePos = st + mouse;
 	float c = circle(circlePos, .01, 2.);
 
-    float r1 = texture2D(uTexture, vUv + .3  * normSpeed * c / 4.).r;
-  float g1 = texture2D(uTexture, vUv + .2  *normSpeed *  c / 4.).g;
-  float b1 = texture2D(uTexture, vUv + .1  * normSpeed *c / 4.).b;
+    float r1 = texture2D(uTexture, vUv + .3  * normSpeed * c / 6.).r;
+  float g1 = texture2D(uTexture, vUv + .2  *normSpeed *  c / 6.).g;
+  float b1 = texture2D(uTexture, vUv + .1  * normSpeed *c / 6.).b;
 
 
   vec3 texture = vec3(r1, g1 ,b1);
